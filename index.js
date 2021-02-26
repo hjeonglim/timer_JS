62d0fd74a8f4379a8604746693f0bc400021f584
Hannah Lim <hjeong.lim@gmail.com>
2021-02-25 16:44:36 -0800

    Add svg and update js files to animate the timer

--- index.js
+++ index.js
@@ -1,66 +1,23 @@
-class Timer {
-    constructor (durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks){
-            this.onStart = callbacks.onStart;
-            this.onTick = callbacks.onTick;
-            this.onComplete = callbacks.onComplete;
-        }
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
-        this.startButton.addEventListener("click", this.start);
-        this.pauseButton.addEventListener("click", this.pause);
-    }
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
-    start = () => {
-        if (this.onStart) {
-            this.onStart();
-        }
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-    }
-
-    pause = () => {
-        clearInterval(this.interval);
-    }
-
-    tick = () => {
-        if (this.durationInput.value <= 0) {
-            this.pause();
-            if (this.onComplete) {
-                this.onComplete();
-            } 
-        }
-        else {
-            this.timeRemaining = this.timeRemaining - 1;
-            if (this.onTick) {
-                this.onTick();
-            } 
-        }
-    }
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-}
-
-const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector("#start");
-const pauseButton = document.querySelector("#pause");
-
-timer = new Timer(durationInput, startButton, pauseButton,{
-    onStart() {
-        console.log('Timer started.')
-    },
-    onTick() {
-        console.log('Timer just ticked down.')
-    },
-    onComplete() {
-        console.log('Timer is compeleted.')
-    }
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  }
 });
